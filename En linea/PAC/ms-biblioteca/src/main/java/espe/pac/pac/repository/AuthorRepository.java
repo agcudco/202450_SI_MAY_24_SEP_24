9651cea38a1b7dd11aba6c2e492c3b1378dff67f
agcudco <agcudco@espe.edu.ec>
2024-07-23 16:05:45 -0500

    correccion pac

--- En linea/PAC/ms-biblioteca/src/main/java/espe/pac/pac/repository/AuthorRepository.java
+++
@@ -1,9 +0,0 @@
-package espe.pac.pac.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import espe.pac.pac.model.Author;
-
-public interface AuthorRepository extends JpaRepository<Author, Long>{
-
-}
